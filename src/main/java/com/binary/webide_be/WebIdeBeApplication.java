48e60206e104f882627b340c3626ec2458d49d75
Jjuhui313 <hyacinth92928@gmail.com>
2024-03-11 15:42:34 +0900

    Docs: 초기 설정

---
+++ src/main/java/com/binary/webide_be/WebIdeBeApplication.java
@@ -0,0 +1,13 @@
+package com.binary.webide_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIdeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebIdeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binary/webide_be/WebIdeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary.webide_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
