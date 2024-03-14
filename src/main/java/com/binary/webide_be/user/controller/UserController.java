4c68c3104f67ff3aa9f61487a01cbba00a092ac3
Jjuhui313 <hyacinth92928@gmail.com>
2024-03-14 19:18:19 +0900

    [BW-11]Util: swagger 적용

--- src/main/java/com/binary/webide_be/user/controller/UserController.java
+++ src/main/java/com/binary/webide_be/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.binary.webide_be.user.dto.LoginRequestDto;
 import com.binary.webide_be.user.dto.SignupRequestDto;
 import com.binary.webide_be.user.service.UserService;
 import com.binary.webide_be.util.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ public class UserController {
     private final UserService userService;
 
     //회원가입
+    @Operation(summary = "회원가입", description = "[회원가입] api")
     @PostMapping("/signup")
     public ResponseEntity<ResponseDto<?>> signup(
             @RequestBody @Valid SignupRequestDto signupRequestDto
@@ -29,12 +31,14 @@ public class UserController {
     }
 
     //이메일 중복 체크
+    @Operation(summary = "회원가입 - 이메일 중복 체크", description = "[이메일 중복 체크] api")
     @PostMapping("/signup/{email}")
     public ResponseEntity<ResponseDto<?>> checkEmail(@PathVariable @CustomEmail String email) {
         return ResponseEntity.ok(userService.emailCheck(email));
     }
 
     //로그인
+    @Operation(summary = "일반 로그인", description = "[일반 로그인] api")
     @PostMapping("/login")
     public ResponseEntity<ResponseDto<?>> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
         return ResponseEntity.ok(userService.login(loginRequestDto, response));
