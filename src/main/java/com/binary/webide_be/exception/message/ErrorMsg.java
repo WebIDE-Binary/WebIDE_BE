d06d530b233184e57734f24d501ee1e986f4f14c
Lee-Siny <sinyoung.siny@gmail.com>
2024-03-18 21:42:53 +0900

    [BW-32]Feat: Chat 관련 Error, Succese Message 추가

--- src/main/java/com/binary/webide_be/exception/message/ErrorMsg.java
+++ src/main/java/com/binary/webide_be/exception/message/ErrorMsg.java
@@ -21,6 +21,7 @@ public enum ErrorMsg {
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     USER_NOT_FOUND(NOT_FOUND, "사용자가 존재하지 않습니다."),
+    CHAT_ROOM_NOT_FOUND(NOT_FOUND, "채팅방이 존재하지 않습니다."),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_USER(CONFLICT,"이미 가입된 사용자입니다."),
