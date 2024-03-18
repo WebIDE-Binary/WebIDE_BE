e390dd0c00f163c8f77efeff417be0f8d4e0ffbc
Lee-Siny <sinyoung.siny@gmail.com>
2024-03-18 20:24:20 +0900

    [BW-28]Feat: Chat관련 Repository 추가

---
+++ src/main/java/com/binary/webide_be/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,15 @@
+package com.binary.webide_be.chat.repository;
+
+import com.binary.webide_be.chat.entity.ChatMessage;
+import com.binary.webide_be.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    @EntityGraph(attributePaths = {"sender"})
+    List<ChatMessage> findByChatRoomId(ChatRoom chatRoomId);
+}
