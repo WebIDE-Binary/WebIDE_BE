6d2a4e804628830f91d06ebcbcf4f41cb12308c0
siontext <topaegr3525@kakao.com>
2024-03-22 00:51:41 +0900

    [BW-67] 내 프로젝트 목록 조회 api 개발

--- src/main/java/com/binary/webide_be/team/repository/UserTeamRepository.java
+++ src/main/java/com/binary/webide_be/team/repository/UserTeamRepository.java
@@ -5,10 +5,14 @@ import com.binary.webide_be.team.entity.UserTeam;
 import com.binary.webide_be.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
     //사용자와 팀을 기반으로 UserTeam 엔티티를 찾는 메서드
     Optional<UserTeam> findByUserAndTeam(User user, Team team);
+
+    //user엔티티 인스턴스에 해당하는 모든 UserTeam엔티티를 데이터 베이스에서 조회함 -> User엔티티가 참여하고 있는 UserTeam관계목록을 반환함 -> UserTeam 목록을 통해 해당 사용자가 속한 모든 팀의 정보를 알 수 있다.
+    List<UserTeam> findByUser(User user);
 }
