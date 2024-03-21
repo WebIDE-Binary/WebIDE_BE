6dfdac204a68da96aeb7097758a36c894aed4106
GabinPark <loveqls1004@gmail.com>
2024-03-21 10:46:02 +0900

    [BW-22][BW-26]Chore: teamService에 따른 controller, entity 변경

--- src/main/java/com/binary/webide_be/team/controller/TeamController.java
+++ src/main/java/com/binary/webide_be/team/controller/TeamController.java
@@ -1,17 +1,18 @@
 package com.binary.webide_be.team.controller;
 
+import com.binary.webide_be.security.UserDetailsImpl;
 import com.binary.webide_be.team.dto.*;
 import com.binary.webide_be.team.entity.Team;
 import com.binary.webide_be.team.service.TeamService;
-import com.binary.webide_be.user.dto.SignupRequestDto;
+import com.binary.webide_be.user.entity.User;
 import com.binary.webide_be.util.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,9 +24,10 @@ public class TeamController {
     //팀 생성
     @Operation(summary = "팀 생성", description = "[팀 생성] api")
     @PostMapping("/api/teams")
-    public ResponseEntity<Team> createTeam (@RequestBody CreateRequestDto createRequestDto){
+    public ResponseEntity<ResponseDto<?>> createTeam (@RequestBody @Valid CreateRequestDto<?> createRequestDto,
+                                                      @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails){
 
-        return ResponseEntity.ok(teamService.createTeam()); // 유저 디테일 필요함
+        return ResponseEntity.ok(teamService.createTeam(createRequestDto, userDetails)); // 유저 디테일 필요함
         //그럼 제가 이렇게 적은건 유저검증을 거치지 않고 팀생성을 하게끔 만든걸까요??
 
     }
@@ -34,19 +36,30 @@ public class TeamController {
     @Operation(summary = "팀 수정", description = "[팀 수정] api")
 
     @PatchMapping("/api/teams/{teamId}")
-    public  ResponseEntity<Team> modifyTeam (ModifyRequestDto modifyRequestDto, HttpServletResponse response) {
+    public ResponseEntity<ResponseDto> modifyTeam (ModifyRequestDto modifyRequestDto,
+                                                   @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        return ResponseEntity.ok((Team) teamService.modifiedTeam(modifyRequestDto, response));
+        return ResponseEntity.ok(teamService.modifiedTeam(modifyRequestDto, userDetails));
     }
 
     // 팀 관리
-    @GetMapping("/api/teams")
-
-    public  ResponseEntity<Team> manageTeam (@RequestBody ManageRequestDto manageRequestDto, HttpServletResponse response) {
-
-        ManageRequestDto manageResponseDto = new ManageRequestDto();
-        return ResponseEntity.ok(teamService.manageTeam(manageResponseDto, response));
-    }
+//    @Operation(summary = "팀 관리 - 검색 기능", description = "[팀 관리 - 검색기능] api")
+//    @GetMapping("/api/teams/find?search={email or nickname}")
+//
+//    public ResponseEntity<ResponseDto> manageTeam (@RequestBody ManageRequestDto manageRequestDto,
+//                                                   @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+//
+//        ManageRequestDto manageResponseDto = new ManageRequestDto();
+//        return ResponseEntity.ok(teamService.manageTeam(manageResponseDto, userDetails));
+//
+//
+//    }
+    //팀 검색
+//    public ResponseEntity<ResponseDto> searchParticipants(@RequestParam String keyword, ManageRequestDto manageRequestDto,
+//                                                          @Param) {
+//        List<User> participants = userService.searchParticipants(keyword);
+//        return ResponseEntity.ok(participants);
+//    }
 
 }
 
