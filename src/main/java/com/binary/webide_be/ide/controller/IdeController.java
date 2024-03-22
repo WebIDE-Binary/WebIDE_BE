9de1405b2b7ca77cf2e7d7ac92ab44e050aa1c3b
Lee-Siny <sinyoung.siny@gmail.com>
2024-03-22 14:35:51 +0900

    [BW-82]Refactor: IdeController의 어노테이션 변경

--- src/main/java/com/binary/webide_be/ide/controller/IdeController.java
+++ src/main/java/com/binary/webide_be/ide/controller/IdeController.java
@@ -8,12 +8,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/ide")
 public class IdeController {
